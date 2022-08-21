52fe5f43958d2daab517233eb580167f72610c12
David Purdy <davidpurdy@gmail.com>
2022-08-20 19:19:28 -0700

    Add build scripts

---
+++ dist/blocks.build.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '08c6b9fbe461b8bedb7c');
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * External Dependencies
+ */
+ const path = require( 'path' );
+ 
+ const { resolve } = require( 'path' );
+ /**
+  * WordPress Dependencies
+  */
+ const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
+ 
+ module.exports = ( env ) => {
+
+    return {
+        ...defaultConfig,
+        ...{
+                mode: env.dev ? 'development' : 'production',
+                entry: {
+                    blocks: './src/blocks.js'
+                },
+                output: {
+                    filename: '[name].build.js',
+                    path: resolve( process.cwd(), 'dist' )
+                }
+            }
+        }
+ }
